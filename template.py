a3b55c1e5f79ec629cf61e54e244d135246c2e2e
Adeliyio <adeliyitomiwa@yahoo.com>
2024-03-16 19:48:41 +0000

    Project Template Updated

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "Tennis Object Detection"
+project_name = "TODetection"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
