c0c76674fa05d246cbe6fd163b01593eebed7e99
Adeliyio <adeliyitomiwa@yahoo.com>
2024-03-16 20:29:22 +0000

    Requirements Added and Setup Done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Tennis Object Detection',
+    version= '0.0.0',
+    author= 'Tommy Adeliyi',
+    author_email= 'oadatascientist@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
